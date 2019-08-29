b903465741cf0852e071ae1bae519e00f4b6c563
blehrer99 <benjaminrlehrer@gmail.com>
2019-08-29 14:48:54 -0400

    fix manual input

--- Arduino/arcade_code/arcade/arcade.ino
+++ Arduino/arcade_code/arcade/arcade.ino
@@ -31,140 +31,147 @@ const unsigned char HomeEncodedState = 0b01010011;  //set home position to top l
 //---------------------------
 
 void setup() {
-    initPins();
+  initPins();
 
-    Serial.begin(9600);
-    Sergio.begin(9600);
+  Serial.begin(9600);
+  Sergio.begin(9600);
 }
 
 //---------------------------
 
 void initPins() {
-    pinMode(MagPin, INPUT);
+  pinMode(MagPin, INPUT);
 
-    pinMode(VertUp, INPUT);
-    pinMode(VertDown, INPUT);
+  pinMode(VertUp, INPUT);
+  pinMode(VertDown, INPUT);
 
-    pinMode(HorLeft, INPUT);
-    pinMode(HorRight, INPUT);
+  pinMode(HorLeft, INPUT);
+  pinMode(HorRight, INPUT);
 
-    pinMode(ActForward, INPUT);
-    pinMode(ActBack, INPUT);
+  pinMode(ActForward, INPUT);
+  pinMode(ActBack, INPUT);
 
-    pinMode(ResetPin, INPUT);
+  pinMode(ResetPin, INPUT);
 
-    pinMode(Unused10, INPUT);
+  pinMode(Unused10, INPUT);
 }
 
 //---------------------------
 
 void loop() {
-    if (Serial.available())
-        checkManualInput();
+  if (Serial.available())
+    checkManualInput();
 
-    /*
+  /*
     encode data as unsigned char from arcade as follows:
-        format: item (off/on 0/1)
+      format: item (off/on 0/1)
 
-        mag|up|down|left|right|forward|back|reset
-        7  |6 |5   |4   |3    |2      |1   |0
-    */
+      mag|up|down|left|right|forward|back|reset
+      7  |6 |5   |4   |3    |2      |1   |0
+  */
 
-    //if it's been longer than the interval, update Sergio
-    if ((millis() - prevSendTime >= UpdateInterval))
-        sendCommands(checkControls());
+  //if it's been longer than the interval, update Sergio
+  if ((millis() - prevSendTime >= UpdateInterval))
+    sendCommands(checkControls());
 }
 
 //---------------------------
 
 void checkManualInput() {
-    String input = Serial.readString();
+  String input = Serial.readString();
 
-    if (input == "manual")
-        autoMode = false;
+  if (input == "manual")
+    autoMode = false;
 
-    if (input == "auto") {
-        autoMode = true;
-        lastManualState = 0;
+  if (input == "auto") {
+    autoMode = true;
+    lastManualState = 0;
+  }
+
+  if (input.length() == 8) {  //if inputing binary encoded state, handle
+    unsigned char encodedState = 0;
+    for (int i = 0; i < 8; ++i) {
+      int x = input.charAt(i) - '0';
+      if (x != 0 && x != 1) {
+        return;
+      }
+      if (x == 1) {
+        encodedState |= (1 << (7 - i));
+      }
     }
 
-    if (input.length() == 8) {  //if inputing binary encoded state, handle
-        unsigned char encodedState = 0;
-        for (int i = 0; i < 8; ++i) {
-            int x = input.charAt(i) - '0';
-            if (x != 0 && x != 1)
-                return;
-            encodedState |= (x << i);
-        }
-
-        //could run checks on data in the future
-
-        lastManualState = encodedState;
-        sendCommands(encodedState);
+    for (int i = 0; i < 8; ++i) {
+      Serial.print((encodedState & (1 << (7 - i))) ? "1" : "0");
     }
+
+    //could run checks on data in the future
+
+    lastManualState = encodedState;
+    sendCommands(encodedState);
+  }
 }
 
 //---------------------------
 
 //args unsigned char encodedState - see comment in loop()
 void sendCommands(unsigned char encodedState) {
-    prevSendTime = millis();
-    Sergio.write(encodedState);  //send the encoded state of the controls to Sergio
+  prevSendTime = millis();
+  Sergio.write(encodedState);  //send the encoded state of the controls to Sergio
 }
 
 //---------------------------
 
 //check the position of the controls and their legality
 unsigned char checkControls() {
-    unsigned char encodedState = autoMode ? 0 : lastManualState;
+  unsigned char encodedState = autoMode ? 0 : lastManualState;
 
-    if (autoMode)
-        encodedState = findEncodedState();
+  if (autoMode)
+    encodedState = findEncodedState();
 
-    if (checkMagTimeout()) {        //if the mag has not timed out, turn off
-        encodedState &= ~(1 << 7);  //set the mag bit to 0
-        prevMagTime = millis();
-    }
+  if (checkMagTimeout()) {        //if the mag has not timed out, turn off
+    encodedState &= ~(1 << 7);  //set the mag bit to 0
+    prevMagTime = millis();
+  }
 
-    return encodedState;
+  return encodedState;
 }
 
 //---------------------------
 
 bool checkMagTimeout() {
-    //if the magnet has been on for longer than the allowed interval, timeout
-    if (millis() - prevMagTime >= MagInterval)
-        return true;
-    return false;
+  //if the magnet has been on for longer than the allowed interval, timeout
+  if (millis() - prevMagTime >= MagInterval)
+    return true;
+  return false;
 }
 
 //---------------------------
 
 unsigned char findEncodedState() {
-    unsigned long encodedState = 0;
+  unsigned long encodedState = 0;
 
-    if (digitalRead(ResetPin) == HIGH)
-        return HomeEncodedState;  //if reset is pressed, return to home
+  if (digitalRead(ResetPin) == HIGH)
+    return HomeEncodedState;  //if reset is pressed, return to home
 
-    if (digitalRead(MagPin) == HIGH)
-        encodedState |= (1 << 7);
-    else
-        prevMagTime = millis();
+  if (digitalRead(MagPin) == HIGH)
+    encodedState |= (1 << 7);
+  else
+    prevMagTime = millis();
 
-    if (digitalRead(VertUp) == HIGH)
-        encodedState |= 6;
-    else if (digitalRead(VertDown) == HIGH)
-        encodedState |= 5;
+  if (digitalRead(VertUp) == HIGH)
+    encodedState |= 6;
+  else if (digitalRead(VertDown) == HIGH)
+    encodedState |= 5;
 
-    if (digitalRead(HorLeft) == HIGH)
-        encodedState |= 4;
-    else if (digitalRead(HorRight) == HIGH)
-        encodedState |= 3;
+  if (digitalRead(HorLeft) == HIGH)
+    encodedState |= 4;
+  else if (digitalRead(HorRight) == HIGH)
+    encodedState |= 3;
 
-    if (digitalRead(ActForward) == HIGH)
-        encodedState |= 2;
-    else if (digitalRead(ActBack) == HIGH)
-        encodedState |= 1;
+  if (digitalRead(ActForward) == HIGH)
+    encodedState |= 2;
+  else if (digitalRead(ActBack) == HIGH)
+    encodedState |= 1;
 
-    return encodedState;
+  return encodedState;
 }
